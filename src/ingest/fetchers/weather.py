567c607cad6c92d1be080f49303f323d2d4973c1
axel-samaniego <axel.samaniego@gmail.com>
2025-07-29 20:25:53 -0400

    made weather send one hwhole thing

--- src/ingest/fetchers/weather.py
+++ src/ingest/fetchers/weather.py
@@ -20,6 +20,7 @@ NY_ZONES = {
 
 async def poll(feed_cfg, session: ClientSession, producer: AIOKafkaProducer):
     while True:
+        payload = {}
         try:
             t0 = time.time()
             base_url = feed_cfg["url"]
@@ -27,20 +28,20 @@ async def poll(feed_cfg, session: ClientSession, producer: AIOKafkaProducer):
                 r = await session.get(base_url.format(zone=zone), timeout=5)
                 data = await r.json()
                 current_forecast = data["properties"]["periods"][0]["detailedForecast"]
-                json_data = {
+                payload[bourough] = {
                     "forecast": current_forecast,
-                    "bourough": bourough,
                     "weather_zone": zone,
                     "timestamp": datetime.now().isoformat(),
                 }
-                payload = gzip.compress(json.dumps(json_data).encode())
-                await producer.send_and_wait(
-                    topic=feed_cfg["topic"],
-                    key=feed_cfg["name"].encode(),
-                    value=payload,
-                    timestamp_ms=int(t0 * 1000),
-                )
-                logger.info(f"Successfully fetched and sent data for {bourough}")
+
+            payload = gzip.compress(json.dumps(payload).encode())
+            await producer.send_and_wait(
+                topic=feed_cfg["topic"],
+                key=feed_cfg["name"].encode(),
+                value=payload,
+                timestamp_ms=int(t0 * 1000),
+            )
+            logger.info(f"Successfully fetched and sent data for {bourough}")
         except Exception as e:
             logger.error(f"Error in {feed_cfg['name']}: {e}")
         finally:
